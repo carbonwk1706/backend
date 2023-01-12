1ea324f09d16b446411bc849e2cb3915c0848a06
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-01-12 12:30:31 +0700

    Encrypt Password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { ROLE } = require('../constant')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 const { Schema } = mongoose
 const userSchema = Schema({
   name: String,
@@ -8,6 +9,29 @@ const userSchema = Schema({
   roles: {
     type: [String],
     default: [ROLE.USER]
+  }
+})
+
+userSchema.pre('save', function (next) {
+  const user = this
+
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(10, function (saltError, salt) {
+      if (saltError) {
+        return next(saltError)
+      } else {
+        bcrypt.hash(user.password, salt, function (hashError, hash) {
+          if (hashError) {
+            return next(hashError)
+          }
+
+          user.password = hash
+          next()
+        })
+      }
+    })
+  } else {
+    return next()
   }
 })
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
 const { generateAccessToken } = require('../helpers/auth')
 
 const login = async function (req, res, next) {
   const username = req.body.username
   const password = req.body.password
   try {
-    const user = await User.findOne({ username, password }, '-password').exec()
-    if (user === null) {
+    const user = await User.findOne({ username }).exec()
+    const verifyResult = await bcrypt.compare(password, user.password)
+    if (!verifyResult) {
       return res.status(404).json({
         message: 'User not found!!'
       })
     }
     const token = generateAccessToken({ _id: user.id, username: user.username })
-    res.json({ user, token })
+    res.json({ user: { _id: user._id, username: user.username, roles: user.roles }, token })
   } catch (err) {
     return res.status(404).send({
       message: err.message
