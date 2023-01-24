fd5bfe652839b3eef105cb2fd65b4628255e7379
puchong <puchong2543@gmail.com>
2023-01-24 11:55:00 +0700

    Add update and delete route

--- routes/books.js
+++ routes/books.js
@@ -49,7 +49,44 @@ const addBooks = async function (req, res, next) {
   }
 }
 
+const updateBook = async function (req, res, next) {
+  const bookId = req.params.id
+  try {
+    const book = await Book.findById(bookId).exec()
+    book.name = req.body.name
+    book.auther = req.body.auther
+    book.publisher = req.body.publisher
+    book.catagory = req.body.catagory
+    book.price = req.body.price
+    book.imageBook = req.body.imageBook
+    await book.save()
+    return res.status(200).json(book)
+  } catch (err) {
+    return res.status(404).send({ message: err.message })
+  }
+}
+
+const deleteBook = async function (req, res, next) {
+  const bookId = req.params.id
+  try {
+    const book = await Book.findById(bookId).exec()
+    if (book === null) {
+      return res.status(404).json({
+        message: 'Book not found!!'
+      })
+    }
+    await Book.findByIdAndDelete(book)
+    return res.status(200).send()
+  } catch (err) {
+    return res.status(404).send({
+      message: err.message
+    })
+  }
+}
+
 router.get('/', getBooks)
 router.get('/:id', getBook)
 router.post('/', addBooks)
+router.put('/:id', updateBook)
+router.delete('/:id', deleteBook)
 module.exports = router
