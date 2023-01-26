20b24afc4773cea4ae27947bd4ff676c09bb7a17
puchong <puchong2543@gmail.com>
2023-01-27 00:06:05 +0700

    Change schema type in bookSchema

--- models/Book.js
+++ models/Book.js
@@ -1,13 +1,35 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 const bookSchema = Schema({
-  name: String,
-  author: String,
-  publisher: String,
-  category: String,
-  price: Number,
-  imageBook: String,
-  sales: Number
+  name: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  publisher: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  imageBook: {
+    type: String,
+    required: true
+  },
+  sales: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 module.exports = mongoose.model('Book', bookSchema)
