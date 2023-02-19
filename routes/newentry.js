5cde0e54f17a1312569839eac91e6e114e6828e1
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-02-20 00:20:32 +0700

    get category newentry

--- routes/newentry.js
+++ routes/newentry.js
@@ -7,5 +7,19 @@ const newEntry = async function (req, res, next) {
   res.json(books)
 }
 
+const getNewEntryCartoon = async function (req, res, next) {
+  const category = 'การ์ตูนทั่วไป'
+  const books = await Book.find().find({ category }).sort({ createdAt: -1 })
+  res.json(books)
+}
+
+const getNewEntryNovel = async function (req, res, next) {
+  const category = 'นิยาย'
+  const books = await Book.find().find({ category }).sort({ createdAt: -1 })
+  res.json(books)
+}
+
 router.get('/', newEntry)
+router.get('/category/cartoon', getNewEntryCartoon)
+router.get('/category/novel', getNewEntryNovel)
 module.exports = router
