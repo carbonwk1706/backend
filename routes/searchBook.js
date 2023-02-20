274105e37b93f51bf14398f654525b45a6a4b965
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-02-20 11:33:59 +0700

    add search match name book

--- routes/searchBook.js
+++ routes/searchBook.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Book = require('../models/Book')
+const User = require('../models/User')
 
 const searchBook = async function (req, res, next) {
   const { searchTerm } = req.query
@@ -15,5 +16,26 @@ const searchBook = async function (req, res, next) {
   const books = await Book.find(searchQuery)
   res.send(books)
 }
+const searchInventory = async function (req, res, next) {
+  const { searchTerm } = req.query
+  console.log(typeof searchTerm)
+
+  const searchQuery = {
+    name: new RegExp(searchTerm, 'i')
+  }
+
+  const userId = req.query.userId
+  console.log('userId:', userId)
+
+  const user = await User.findById(userId).populate('inventory')
+  console.log('user:', user)
+
+  const books = user.inventory.filter(book => new RegExp(searchQuery.name, 'i').test(book.name))
+  console.log('books:', books)
+
+  res.send(books)
+}
+
 router.get('/', searchBook)
+router.get('/inventory', searchInventory)
 module.exports = router
