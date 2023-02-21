d052e9bd33dc4ef4dcec3e38606c4b5928d6f2c0
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-02-21 12:07:12 +0700

    check recommend in 7 day

--- routes/recommended.js
+++ routes/recommended.js
@@ -6,7 +6,9 @@ const recommended = async function (req, res) {
   Book.find()
     .sort({ ratingsCount: -1, rating: -1 })
     .then((books) => {
-      res.status(200).send(books)
+      const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)
+      const newBooks = books.filter(book => book.updatedAt >= sevenDaysAgo)
+      res.status(200).send(newBooks)
     })
     .catch((error) => {
       res.status(500).send({ message: error.message })
