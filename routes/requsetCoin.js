92ac832acbdaeb1b1c78e99608eb72f91e61e3cb
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-02-23 13:38:42 +0700

    approve and reject coin

--- routes/requsetCoin.js
+++ routes/requsetCoin.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Receipt = require('../models/Receipt')
+const User = require('../models/User')
 
 const getRequests = async function (req, res, next) {
   try {
@@ -21,6 +22,37 @@ const getRequest = async function (req, res, next) {
   }
 }
 
+const approveRequest = async function (req, res, next) {
+  const id = req.params.id
+  try {
+    const request = await Receipt.findById(id)
+    if (!request) return res.status(404).send('Request not found')
+    request.status = 'approved'
+    await request.save()
+    const user = await User.findById(request.user)
+    user.coin += request.amount
+    await user.save()
+    res.send({ request, user })
+  } catch (error) {
+
+  }
+}
+
+const rejectRequest = async function (req, res, next) {
+  const id = req.params.id
+  try {
+    const request = await Receipt.findById(id)
+    if (!request) return res.status(404).send('Request not found')
+    request.status = 'rejected'
+    await request.save()
+    res.send({ request })
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+
 router.get('/', getRequests)
 router.get('/:id', getRequest)
+router.patch('/:id/approve', approveRequest)
+router.patch('/:id/reject', rejectRequest)
 module.exports = router
