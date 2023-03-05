a97a089ffebb10d7acc0d6314624d52c8a833c3e
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-03-05 23:41:20 +0700

    add pdf in model book

--- models/Book.js
+++ models/Book.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 const bookSchema = Schema({
+  pdf: {
+    type: String,
+    required: true
+  },
   name: {
     type: String,
     required: true
