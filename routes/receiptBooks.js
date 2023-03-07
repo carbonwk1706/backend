20ef06ebd73bdc5da5ab30e9ba2aa90e803e550e
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-03-07 20:54:46 +0700

    when book delete find in mode DeletedBook

--- routes/receiptBooks.js
+++ routes/receiptBooks.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const Book = require('../models/Book')
+const DeletedBook = require('../models/DeletedBook')
 
 const getReceiptBooks = async function (req, res) {
   const userId = req.params.userId
@@ -19,7 +21,26 @@ const receiptBookDetail = async function (req, res) {
   const userId = req.params.userId
   const user = await User.findById(userId).populate('receiptBooks.books')
   const receiptBook = user.receiptBooks.find(receiptBook => receiptBook._id.toString() === receiptBookId)
-  res.send(receiptBook)
+  if (!receiptBook) {
+    return res.status(404).send({ error: 'Receipt book not found' })
+  }
+
+  const books = []
+  for (let i = 0; i < receiptBook.oldData.length; i++) {
+    const book = receiptBook.oldData[i]
+    const foundBook = await Book.findById(book._id)
+    if (foundBook) {
+      books.push(foundBook)
+    } else {
+      const deletedBook = await DeletedBook.findOne({ bookId: book._id })
+      if (deletedBook) {
+        books.push(deletedBook)
+      }
+    }
+  }
+  res.send({
+    books, receiptBook
+  })
 }
 
 router.get('/:userId', getReceiptBooks)
