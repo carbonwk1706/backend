8676061182d1cbee8cf1a06df1b6f72b52093f7e
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-03-10 03:45:54 +0700

    add model RequestBook

---
+++ models/RequestBook.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+const RequestBookSchema = Schema({
+  request: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'approved', 'rejected'],
+    default: 'pending'
+  },
+  approvedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  approvedAt: { type: Date },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  pdf: {
+    type: String
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  publisher: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  imageBook: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('RequestBook', RequestBookSchema)
