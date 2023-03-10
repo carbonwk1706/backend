cd6b04ab542dc268b406e62b9afa318d83344620
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-03-10 13:11:53 +0700

    all request add ruquestbook

--- routes/allRequest.js
+++ routes/allRequest.js
@@ -2,12 +2,14 @@ const express = require('express')
 const router = express.Router()
 const Receipt = require('../models/Receipt')
 const Request = require('../models/Request')
+const RequestBook = require('../models/RequestBook')
 
 const getRequestsAndReceipts = async function (req, res, next) {
   try {
     const requests = await Request.find({ status: 'pending' }).sort({ createdAt: 1 })
     const receipts = await Receipt.find({ status: 'pending' }).sort({ createdAt: 1 })
-    const combinedData = requests.concat(receipts)
+    const requestBook = await RequestBook.find({ status: 'pending' }).sort({ createdAt: 1 })
+    const combinedData = requests.concat(receipts, requestBook)
     combinedData.sort((a, b) => {
       return new Date(a.createdAt) - new Date(b.createdAt)
     })
