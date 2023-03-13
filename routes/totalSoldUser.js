19453a255ef760d2c61ae1fbe02c91b4c71eab05
Khun CAPTAIN <mrcaptanch@gmail.com>
2023-03-13 07:54:54 +0700

    add get balance user sell

--- routes/totalSoldUser.js
+++ routes/totalSoldUser.js
@@ -12,7 +12,7 @@ const getUserTotalSoldAndRevenue = async function (req, res) {
     user.bookSell.forEach(book => {
       if (book.sold > 0) {
         totalSold += book.sold
-        totalRevenue += book.sold * book.price
+        totalRevenue += (book.sold * book.price) - 5
       }
     })
     user.totalSold = totalSold
@@ -24,21 +24,18 @@ const getUserTotalSoldAndRevenue = async function (req, res) {
   }
 }
 
-const withdrawRevenue = async function (req, res) {
+const getBalance = async function (req, res) {
   try {
     const userId = req.params.userId
-    const amount = req.body.amount
     const user = await User.findById(userId).exec()
     if (!user) return res.status(404).send('User not found')
-    if (user.totalRevenue < amount) return res.status(400).send('Not enough revenue')
-    user.totalRevenue -= amount
-    await user.save()
-    res.status(200).json({ user, totalRevenue: user.totalRevenue })
+    const balance = user.totalRevenue - user.balance
+    res.status(200).json({ balance, idAccount: user.idAccount, bankAccount: user.bankAccount })
   } catch (error) {
     res.status(500).send(error)
   }
 }
 
 router.get('/:userId', getUserTotalSoldAndRevenue)
-router.put('/:userId/withdraw', withdrawRevenue)
+router.get('/balance/:userId', getBalance)
 module.exports = router
